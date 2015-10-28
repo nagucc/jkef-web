30dda69e754f952e9af6a6558fa5550d6635949b
na57 <na57@qq.com>
2015-10-28 11:31:53 +0800

    使用promise，避免多次ajax请求

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -8,11 +8,24 @@
  * Controller of the jkefWebApp
  */
 angular.module('jkefWebApp')
-  .controller('HeaderCtrl', function ($scope, auth) {
-    $scope.loginUrl = 'http://aa.com';
-    auth.getAuthorizeUrl(function (data) {
-    	$scope.loginUrl = data;
+  .controller('HeaderCtrl', function ($scope, auth, $rootScope) {
+  	$rootScope.promises = $rootScope.promises || {};
+
+    $rootScope.promises.me = auth.getMe();
+
+    $rootScope.promises.me.then(function (me) {
+    	$rootScope.me = me;
     }, function (err) {
-    	alert(err.msg);
+
+    	// 如果用户未登录
+    	if(err.ret === -2){
+
+    		// 生成登录按钮链接
+    		auth.getAuthorizeUrl().then(function (data) {
+		    	$scope.loginUrl = data;
+		    }, function (err) {
+		    	alert(err.msg);
+		    });
+    	}
     });
   });
